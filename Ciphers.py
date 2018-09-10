ba29259ffc834d314b25d2a5391f3aeb7c5be1c2
Roman <43120116+xxzx6uzm3@users.noreply.github.com>
2018-09-10 23:47:44 +0300

    Update Ciphers.py

--- Ciphers.py
+++ Ciphers.py
@@ -51,6 +51,7 @@ class AbstractCipher:
         """
         Find X and Y in ax + by = gcd(a,b)
         """
+
         if a == 0:
             return (b, 0, 1)
         else:
@@ -62,17 +63,19 @@ class AbstractCipher:
         """
         Modular Multiplicative Inverse
         """
+
         g, x, y = AbstractCipher.extended_gcd(a, m)
         if g != 1:
-            raise Exception('modular inverse does not exist')
+            raise Exception('Modular inverse does not exist')
         else:
             return x % m
 
     @staticmethod
     def binary_pow(a, n):
         """
-        Exponentiation by squaring / Алгоритм швидкого піднесення в степінь
+        Exponentiation by squaring
         """
+
         if n < 0:
             return AbstractCipher.binary_pow(1 / a, -n)
         elif n == 0:
@@ -84,6 +87,21 @@ class AbstractCipher:
         elif n % 2 != 0:
             return a * AbstractCipher.binary_pow(a * a, (n-1) / 2)
 
+    @staticmethod
+    def get_rand_prime(a, b):
+        """
+        :param a: start point
+        :param b: end point
+        """
+
+        rpn = randint(a, b)
+
+        # Fermat theorem
+        while pow(2, rpn - 1, rpn) != 1 or pow(3, rpn - 1, rpn) != 1 or pow(5, rpn - 1, rpn) != 1 or \
+                pow(7, rpn - 1, rpn) != 1:
+            rpn = randint(a, b)
+        return rpn
+
 
 class RSA(AbstractCipher):
     """
@@ -94,7 +112,7 @@ class RSA(AbstractCipher):
         super().__init__(plaintext, ciphertext)
         self.__p = p
         self.__q = q
-        self.__e = e
+        self.__e = e or self.generate_e()
 
     def set_p(self, p):
         self.__p = p
@@ -124,47 +142,79 @@ class RSA(AbstractCipher):
         """
         1 < e < euler AND gcd(e, euler) == 1
         """
-        if self.get_e() != None:
-            return self.__e
-        else:
-            e = randint(2, 25)  # mb not 25, euler
-            while self.gcd(e, self.get_euler()) != 1:
-                e = randint(2, 25)
-            self.set_e(e)
-            return self.get_e()
+
+        e = randint(2, 25)  # mb not 25, euler
+        while self.gcd(e, self.get_euler()) != 1:
+            e = randint(2, 25)
+        self.set_e(e)
+        return self.get_e()
 
     def get_d(self):
         d = self.modular_inverse(self.get_e(), self.get_euler())
         return d
 
-    def keys_file(self):
-        public_key_file = open("public.key", "w")
-        public_key_file.write("{},{}".format(self.generate_e(), self.get_n()))
-        public_key_file.close()
+    def generate_key_pair(self, rsa_type):
+        warning = None
+        if rsa_type == "RSA-2048":
+            binary_key_size = 2048
+            decimal_key_size = 617
+            warning = False
+        elif rsa_type == "RSA-1536":
+            binary_key_size = 1536
+            decimal_key_size = 463
+            warning = False
+        elif rsa_type == "RSA-1024":
+            binary_key_size = 1024
+            decimal_key_size = 309
+            warning = False
+        elif rsa_type == "RSA-896":
+            binary_key_size = 896
+            decimal_key_size = 270
+            warning = False
+        elif rsa_type == "RSA-768":
+            binary_key_size = 768
+            decimal_key_size = 232
+            warning = True
+        elif rsa_type == "RSA-704":
+            binary_key_size = 704
+            decimal_key_size = 212
+            warning = True
+        elif rsa_type == "RSA-576":
+            binary_key_size = 576
+            decimal_key_size = 174
+            warning = True
 
-        private_key_file = open("private.key", "w")
-        private_key_file.write("{},{}".format(self.get_d(), self.get_n()))
-        private_key_file.close()
+        if warning:
+            warn_message = f"Warning : the key size you have chosen ({rsa_type}) can be easily cracked," \
+                            f" don't use it for cryptographic issues."
+        else:
+            warn_message = ' '
+        print(warn_message)
 
-        return ''
+        p_size = int(decimal_key_size / 2)
+        q_size = decimal_key_size - p_size
+
+        p = self.get_rand_prime(10 ** (p_size - 1), 10 ** p_size - 1)
+        q = self.get_rand_prime(10 ** (q_size - 1), 10 ** q_size - 1)
+
+        return {
+            'p': p,
+            'q': q
+        }
 
     def encrypt(self):
-        # ct = self.binary_pow(self.get_plaintext(), self.get_e()) % self.get_n()
-
         ct = pow(self.get_plaintext(), self.get_e(), self.get_n())
         self.set_ciphertext(ct)
         return self.get_ciphertext()
 
     def decrypt(self):
-        # дуже довго рахує
-        # якщо замінити степінь на 100 000 то рахує за 5 сек...
-        # decrypted_text = self.binary_pow(self.get_ciphertext(), self.get_d()) % self.get_n()
-
         return pow(self.get_ciphertext(), self.get_d(), self.get_n())
 
 
 # Wikipedia example
-# x = RSA(111111, p=3557, q=2579, e=3)
+# x = RSA(111111, p=511193910726569874953935919900363873129238346355395814309112665842586420644909876720511,
+        # q=494506884534583558326764414412197999730498705147369083365809132224425698427662508889603)  # e = 3
+x = RSA(111111)
 # print(x.get_n())
 # print(x.get_euler())
 # print(x.get_e())
@@ -172,3 +222,6 @@ class RSA(AbstractCipher):
 # print(x.get_plaintext())
 # print(x.encrypt())
 # print(x.decrypt())
+# print(x.get_rand_prime(25790000, 35570000))
+# print(x.is_prime(31240019))
+print(x.generate_key_pair("RSA-576"))
